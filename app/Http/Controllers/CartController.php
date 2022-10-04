f517c793eb66cee8026ea1c72134d5b5bfa30194
Saharat Thongin <6306021620191@fitm.kmutnb.ac.th>
2022-10-04 09:53:51 +0700

    Update addToCart()

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,7 +10,28 @@ class CartController extends Controller
         return view('cart/index');
     }
         
-        public function addToCart($id) {
+    public function addToCart($id) {
+        $product = Product::find($id);
+
+        $cart_items = Session::get('cart_items');
+        if(is_null($cart_items)) {
+            $cart_items = array();
+        }
+
+        $qty = 0;
+        if(array_key_exists($product->id, $cart_items)) {
+            $qty = $cart_items[$product->id]['qty'];
+        }
+
+        $cart_items[$product['id']] = array( 
+            'id' => $product->id,
+            'code' => $product->code,
+            'name' => $product->name,
+            'price' => $product->price,
+            'image_url' => $product->image_url,
+            'qty' => $qty + 1,
+        );
+        Session::put('cart_items', $cart_items);
         return redirect('cart/view');
     }
 }
